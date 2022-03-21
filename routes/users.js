b1a53ea4506f3b0ffdede9883c9d3fd35db22a8a
Dillon Lee <deeleeTech@outlook.com>
2022-03-21 00:55:36 -0500

    added bycrpt functionality

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient
 var router = express.Router();
+var bcrypt = require('bcrypt');
 require('dotenv').config();
 
 const connectionString = process.env.MONGO_STRING;
@@ -15,23 +16,27 @@ router.get('/login', async function(req, res, next) {
     //      console.log(req.query); //QUERY FROM FRONT END
     //      console.log('Now Searching for Users');
     let clientUsername = req.query.usernameAttempt;
-    let clientPassword = req.query.passwordAttempt;
+    let clientPassword = req.headers.authorization;
     const db = client.db('FactOrCap');
     db.collection('Users').find({ username: clientUsername }).toArray() //MONGO QUERY !!! ATTEMPTS USER !!!
     .then(results => {
       if(results.length > 0){ //#### USER FOUND ##############
         //console.log(results[0]) 
-        let mongoPassword = results[0].password;
-        if(mongoPassword == clientPassword){//oooo SUCCESSFUL PASSWORD ATTEMPT oooooooooo
-          let loginInfo = results[0];
-          let currentDate = new Date(); 
-          loginInfo.lastLogin = currentDate.getTime(); // Adds Milisecond Timestamp for the Client to reference
-          delete loginInfo.password; //REMOVE PASSWORD DATA BEFORE PASSING TO CLIENT
-          res.send({ message: 'successful_login', loginData: loginInfo }) // PASS TO CLIENT **************************>
-        }
-        else{ //oooo INVALID PASSWORD ATTEMPT oooooooooo
-          res.send({ message: 'incorrect_password', loginData: {} }) // PASS TO CLIENT *******************************>
-        }
+        const mongoPassword = results[0].password;
+        bcrypt.compare(clientPassword, mongoPassword, function(err, hashRes) {
+          if (hashRes == false )  res.send({ message: 'incorrect_password', loginData: {} }) // PASS TO CLIENT *******************************>
+          else {//oooo SUCCESSFUL PASSWORD ATTEMPT oooooooooo
+            let loginInfo = results[0];
+            let currentDate = new Date(); 
+            loginInfo.lastLogin = currentDate.getTime(); // Adds Milisecond Timestamp for the Client to reference
+            delete loginInfo.password; //REMOVE PASSWORD DATA BEFORE PASSING TO CLIENT
+            db.collection('SingleGameBets').find({ $or: [{ usernamePosted: clientUsername },{ usernameAccepted: clientUsername }] }).toArray() //MONGO QUERY!!!
+            .then(betResults => {
+              loginInfo.betsData = betResults;
+              res.send({ message: 'successful_login', loginData: loginInfo }) // PASS TO CLIENT **************************>
+            })
+          }
+        });
       }
       else{ //#### USER NOT FOUND ##############
         res.send({ message: 'incorrect_username', loginData: {} }) // PASS TO CLIENT *********************************>
@@ -45,12 +50,16 @@ router.get('/login', async function(req, res, next) {
 
 /* POST NEW USERS */
 router.post('/signup', async function(req, res, next) {
-  MongoClient.connect(connectionString, (err, client) => {
+  MongoClient.connect(connectionString, async (err, client) => {
     if (err) return console.error(err)
-    let clientUsername = req.body.usernameAttempt;
-    let clientPassword = req.body.passwordAttempt;
-    let clientEmail = req.body.emailAttempt;
-    let currentDate = new Date();
+    const clientUsername = req.body.usernameAttempt;
+    const clientEmail = req.body.emailAttempt;
+    const currentDate = new Date();
+
+    const passwordString = req.body.passwordAttempt;
+    const salt = await bcrypt.genSalt(10);
+    const clientPassword = await bcrypt.hash(passwordString, salt);
+
     const loginInfo = {
       username: clientUsername,
       password: clientPassword,
@@ -68,6 +77,7 @@ router.post('/signup', async function(req, res, next) {
       else{ //CREATE NEW USER
         db.collection('Users').insertOne(loginInfo).then(()=>{
           loginInfo.lastLogin = currentDate.getTime() // ADDS LAST LOGIN FOR CLIENT TO REFREENCE
+          loginInfo.betsData = [];
           res.send({ message: 'created_new_user', loginData: loginInfo })
         })
       }
